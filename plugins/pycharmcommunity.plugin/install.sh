9e616ef361f01a0108a650528c06836858e87117
Andrew Jackson <andrewderekjackson@gmail.com>
2016-09-15 07:18:48 +1200

    Adding PyCharm plugin

---
+++ plugins/pycharmcommunity.plugin/install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+dnf -y  copr enable phracek/PyCharm
+
+dnf -y install pycharm-community
---
+++ plugins/pycharmcommunity.plugin/uninstall.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+dnf -y copr disable phracek/PyCharm
+dnf -y remove pycharm-community
